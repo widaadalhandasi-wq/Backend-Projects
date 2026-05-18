9627aca641aaa00c4585549d9f8f4a917e356431
widaad alhandasi <widaadalhandasi@gmail.com>
2026-05-18 08:46:23 +0400

    Entity Framwork Soultion

---
+++ EitityFramworkSoultion/EitityFramworkSoultion/EitityFramwork/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EitityFramwork
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<student> Students { get; set; }
+        public DbSet<courses> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ; Database=Company ; Trusted_Connection=True ; TrustServerCertificate=True");
+        }
+
+    }
+
+}
---
+++ EitityFramworkSoultion/EitityFramworkSoultion/EitityFramwork/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EitityFramwork
+{
+   public class student
+    {
+        public int id {  get; set; }
+        public string name {  get; set; }
+        public int age {  get; set; }
+    }
+}
